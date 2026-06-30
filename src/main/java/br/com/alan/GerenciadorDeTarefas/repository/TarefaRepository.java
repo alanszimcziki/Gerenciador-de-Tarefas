6b9e37b12347fddfea924d4f68c50c8368c8f4cf
Alan <augustoszimcziki@hotmail.com>
2026-06-29 21:05:11 -0300

    feat: adiciona TarefaRepository

---
+++ src/main/java/br/com/alan/GerenciadorDeTarefas/repository/TarefaRepository.java
@@ -0,0 +1,9 @@
+package br.com.alan.GerenciadorDeTarefas.repository;
+
+import br.com.alan.GerenciadorDeTarefas.entity.Tarefa;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TarefaRepository extends JpaRepository<Tarefa, Long> {
+}
